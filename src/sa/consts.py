b4bae95c2d6b210a0e1f699035b652bdde1ab2a1
Tim Savage <tim@savage.company>
2022-11-21 04:30:53 +1100

    Added common constants and settings

--- src/sa/consts.py
+++ src/sa/consts.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from typing import Final
 
 TEXT_ENCODING: Final[str] = "UTF8"
@@ -6,14 +7,19 @@ TEXT_ENCODING: Final[str] = "UTF8"
 DEFAULT_CA_KEY_SIZE: Final[int] = 4096
 """Default size of private key used by CA."""
 
+DEFAULT_CA_VALID_RANGE: Final[timedelta] = timedelta(days=365 * 10)
+"""Default timedelta a CA certificate is valid for."""
+
 DEFAULT_DOMAIN_KEY_SIZE: Final[int] = 2048
-"""Default size of private key used for managed domains."""
+"""Default size of private key used for managed domains; default 10 years"""
+
+DEFAULT_DOMAIN_VALID_RANGE: Final[timedelta] = timedelta(days=365)
+"""Default timedelta a signed certificate is valid for; default 1 year"""
 
 CA_CONFIG_FILE: Final[str] = ".ca-config.toml"
 """Config file for the CA."""
 
 CA_DOMAIN_FOLDER: Final[str] = ".ca"
-
 PRIVATE_KEY_FILE: Final[str] = "private.key.pem"
 SIGNING_REQUEST_FILE: Final[str] = "signing-request.csr.pem"
 SIGNING_EXTENSION_FILE: Final[str] = "signing-request.ext"
