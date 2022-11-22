650e4a26d23850a0042c0d9987fe519a71ec9de4
Tim Savage <tim@savage.company>
2022-11-23 02:34:49 +1100

    Upgrade create domain model

--- src/sa/models.py
+++ src/sa/models.py
@@ -39,39 +39,46 @@ class CreateCA(odin.AnnotatedResource):
         verbose_name="Email Address",
     )
 
+    def __str__(self):
+        return self.name
+
 
 class CreateDomain(odin.AnnotatedResource):
     """All fields required to create a new domain"""
 
     name: str = odin.Options(
-        verbose_name="Name of CA",
+        verbose_name="Name of Domain",
     )
-    alt_names: list[str] = odin.Options(
-        verbose_name="Alternative Names",
-    )
+    # alt_names: list[str] = odin.Options(
+    #     verbose_name="Alternative Names",
+    # )
     country_name: str = odin.Options(
-        empty=True,
+        default=lambda: settings.DEFAULT_COUNTRY_NAME,
         verbose_name="Country Name",
     )
     state_or_province: str = odin.Options(
-        empty=True,
+        default=lambda: settings.DEFAULT_STATE_OR_PROVINCE,
         verbose_name="State or Province",
     )
     locality: str = odin.Options(
         empty=True,
+        default="",
         verbose_name="Locality",
     )
     org_name: str = odin.Options(
         empty=True,
+        default=lambda: settings.DEFAULT_ORG_NAME,
         verbose_name="Organisation Name",
     )
     org_unit_name: str = odin.Options(
         empty=True,
+        default="",
         verbose_name="Organisation Unit Name",
     )
     email_address: odin.types.Email = odin.Options(
-        validators=[],
         empty=True,
+        default=lambda: settings.DEFAULT_EMAIL,
+        validators=[],
         verbose_name="Email Address",
     )
 
