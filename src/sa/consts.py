912a962e07b350b6e1c8e08ed82703a86c28651d
Tim Savage <tim@savage.company>
2022-11-23 00:45:49 +1100

    Normalise settings and consts

--- src/sa/consts.py
+++ src/sa/consts.py
@@ -1,28 +1,33 @@
+"""Self Authority constants."""
 from datetime import timedelta
-from typing import Final
+from typing import Final, Sequence
 
 TEXT_ENCODING: Final[str] = "UTF8"
 """Text encoding used for all string operations."""
 
-DEFAULT_CA_KEY_SIZE: Final[int] = 4096
-"""Default size of private key used by CA."""
-
-DEFAULT_CA_VALID_RANGE: Final[timedelta] = timedelta(days=365 * 10)
-"""Default timedelta a CA certificate is valid for."""
-
-DEFAULT_DOMAIN_KEY_SIZE: Final[int] = 2048
-"""Default size of private key used for managed domains; default 10 years"""
-
-DEFAULT_DOMAIN_VALID_RANGE: Final[timedelta] = timedelta(days=365)
-"""Default timedelta a signed certificate is valid for; default 1 year"""
-
-CA_CONFIG_FILE: Final[str] = ".ca-config.toml"
-"""Config file for the CA."""
+REPO_DOMAIN: Final[str] = ".sa"
+"""Domain for repo."""
 
 CA_DOMAIN: Final[str] = ".ca"
 """Domain for the CA."""
 
+CA_DEFAULT_KEY_SIZE: Final[int] = 4096
+"""Default size of private key used by CA."""
+
+CA_DEFAULT_EXPIRY_DELTA: Final[timedelta] = timedelta(days=365 * 10)
+"""Default timedelta a CA certificate is valid for."""
+
+DOMAIN_DEFAULT_KEY_SIZE: Final[int] = 2048
+"""Default size of private key used for managed domains; default 10 years"""
+
+DOMAIN_DEFAULT_EXPIRY_DELTA: Final[timedelta] = timedelta(days=365)
+"""Default timedelta a signed certificate is valid for; default 1 year"""
+
+CONFIG_FILE: Final[str] = "config.toml"
 PRIVATE_KEY_FILE: Final[str] = "private.key.pem"
 SIGNING_REQUEST_FILE: Final[str] = "signing-request.csr.pem"
 SIGNING_EXTENSION_FILE: Final[str] = "signing-request.ext"
 CERTIFICATE_FILE: Final[str] = "certificate.crt.pem"
+
+RESERVED_DOMAINS: Final[Sequence[str]] = (REPO_DOMAIN, CA_DOMAIN, ".git")
+"""Reserved domain names."""
--- src/sa/default_settings.py
+++ src/sa/default_settings.py
@@ -1,1 +1,7 @@
 """Default setting definition."""
+
+# Default values
+DEFAULT_COUNTRY_NAME = "AU"
+DEFAULT_STATE_OR_PROVINCE = "NSW"
+DEFAULT_ORG_NAME = "Savage Company"
+DEFAULT_EMAIL = "tim@savage.company"
