1cf2fe07649be0b7bcebe4f790ce980698794c2d
Tim Savage <tim@savage.company>
2022-11-23 02:32:39 +1100

    Remove repo

--- src/sa/consts.py
+++ src/sa/consts.py
@@ -5,9 +5,6 @@ from typing import Final, Sequence
 TEXT_ENCODING: Final[str] = "UTF8"
 """Text encoding used for all string operations."""
 
-REPO_DOMAIN: Final[str] = ".sa"
-"""Domain for repo."""
-
 CA_DOMAIN: Final[str] = ".ca"
 """Domain for the CA."""
 
@@ -23,11 +20,11 @@ DOMAIN_DEFAULT_KEY_SIZE: Final[int] = 2048
 DOMAIN_DEFAULT_EXPIRY_DELTA: Final[timedelta] = timedelta(days=365)
 """Default timedelta a signed certificate is valid for; default 1 year"""
 
-CONFIG_FILE: Final[str] = "config.toml"
+CONFIG_FILE: Final[str] = "config.json"
 PRIVATE_KEY_FILE: Final[str] = "private.key.pem"
 SIGNING_REQUEST_FILE: Final[str] = "signing-request.csr.pem"
 SIGNING_EXTENSION_FILE: Final[str] = "signing-request.ext"
 CERTIFICATE_FILE: Final[str] = "certificate.crt.pem"
 
-RESERVED_DOMAINS: Final[Sequence[str]] = (REPO_DOMAIN, CA_DOMAIN, ".git")
+RESERVED_DOMAINS: Final[Sequence[str]] = (CA_DOMAIN, ".git")
 """Reserved domain names."""
